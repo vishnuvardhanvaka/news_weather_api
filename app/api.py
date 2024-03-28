5fd4e5e2394e1e93a26896824a5f2c84221da086
vishnu <vishnu.vaka@solomonsint.com>
2024-03-28 19:11:02 +0530

    made changes

--- app/api.py
+++ app/api.py
@@ -1,35 +1,7 @@
-import requests
-from bs4 import BeautifulSoup
-from datetime import datetime, timedelta
-from fastapi import FastAPI,Form
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
 
+app = FastAPI()
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost:3000",
-    "http://localhost:3000/",
-    "http://192.168.0.128:3000/",
-    "https://miraparentpal.com",
-    "https://www.miraparentpal.com",
-    'https://miraparentpal.vercel.app',
-    'https://inotes-gamma.vercel.app',
-    'https://ai-avatar-live-stream.vercel.app'
-]
-
-app=FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-@app.get('/getWeather')
-def getWeather():
-  return {'success':True,'msg':'Getting weather'}
-
-
-
+@app.get("/", tags=["Root"])
+async def read_root():
+    return {"message": "Welcome to this fantastic app!"}
