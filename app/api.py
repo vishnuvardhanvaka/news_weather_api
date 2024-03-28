136a6449af00000c66dfb795c11ed5058bcea370
vishnu <vishnu.vaka@solomonsint.com>
2024-03-28 20:48:35 +0530

    added corheader

--- app/api.py
+++ app/api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI,Form
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Optional
 import requests
 from bs4 import BeautifulSoup
@@ -6,7 +7,27 @@ from datetime import datetime, timedelta
 import math
 
 
-app = FastAPI()
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost:3000",
+    "http://localhost:3000/",
+    "http://192.168.0.128:3000/",
+    "https://miraparentpal.com",
+    "https://www.miraparentpal.com",
+    'https://miraparentpal.vercel.app',
+    'https://inotes-gamma.vercel.app',
+    'https://ai-avatar-live-stream.vercel.app'
+]
+
+app=FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/", tags=["Root"])
 async def read_root():
