5c18e2f992ac720cb7b3c41da66a1abce32b70a7
vishnu <vishnu.vaka@solomonsint.com>
2024-03-28 19:37:37 +0530

    changed api

--- app/api.py
+++ app/api.py
@@ -1,7 +1,71 @@
 from fastapi import FastAPI
-
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
 app = FastAPI()
 
 @app.get("/", tags=["Root"])
 async def read_root():
     return {"message": "Welcome to this fantastic app!"}
+
+
+# Send a GET request to the URL
+cnnurl = "https://edition.cnn.com/"
+url='https://news.google.com/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JXVnVMVWRDR2dKSlRpZ0FQAQ?hl=en-IN&gl=IN&ceid=IN%3Aen'
+root_url='https://news.google.com'
+response = requests.get(url)
+
+# Parse the HTML content
+soup = BeautifulSoup(response.text, "html.parser")
+
+def getHeadlines():
+  headlines_data=[]
+  headline_boxes=soup.find_all(class_='W8yrY')
+  print(len(headline_boxes))
+
+  for headline_box in headline_boxes:
+    article=headline_box.find(class_='IBr9hb')
+    figure=article.find('figure')
+    img='None'
+    if figure:
+      img=root_url+figure.find('img').get('src').split('=-')[0]
+    # print(img)
+    article_link_container=article.find('a',class_='gPFEn')
+    article_link=root_url+article_link_container.get('href')
+    article_headline=article_link_container.text
+    # print(article_headline)
+    # print(article_link)
+    time=article.find(class_='hvbAAd').text
+    article_datetime=article.find(class_='hvbAAd').get('datetime')
+    article_from=article.find(class_='msvBD zC7z7b')
+    if article_from:
+      article_from=article_from.get('src')
+    else:
+      article_from='None'
+    # print(time)
+    headline={
+        'title':article_headline,
+        'article_link':article_link,
+        'article_from':article_from,
+        'img_url':img,
+        'time_text':time,
+        'datetime':article_datetime
+    }
+    headlines_data.append(headline)
+    # print('----------------------')
+
+  sorted_headlines = sorted(headlines_data, key=lambda x: x['datetime'],reverse=True)
+
+  return sorted_headlines
+
+# latest_news_headlines=getHeadlines()
+# import json
+# prt=json.dumps(latest_news_headlines, indent=4)
+# print(prt)
+
+@app.get('/getLatestHeadlines')
+async def getLatestHeadlines():
+  latest_news_headlines=getHeadlines()
+  return {'success':True,'headlines':latest_news_headlines}
+
+
